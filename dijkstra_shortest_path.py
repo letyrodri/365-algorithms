e167a0a05955417752972bb8460dad1fe898a606
Leticia L. Rodriguez <lrodrig@dc.uba.ar>
2022-10-17 22:45:42 -0300

    adding a test for dijsktra algorithm

--- dijkstra_shortest_path.py
+++ dijkstra_shortest_path.py
@@ -6,26 +6,38 @@ def dijkstra_shortest_path(graph, s):
 	"""
 
 	pq = PriorityQueue()
-	s = set()
+	S = set()
 
 	pq.put( (0,s) )
 
 	parents = [None for x in range(len(graph))]
-	cost = [0 for x in range(len(graph))]
-
+	cost = [float("inf") for x in range(len(graph))]
+	cost[s] = 0
 	
 	while (not pq.empty()):
-		u = pq.get()
-		s.append(u)
-		
-		for (v,w) in graph[u]:
-			pq.put( (w,v) )
+		u = pq.get()[1]
+		if u not in S:
+			S.add(u)
 
-			if cost[v] is None or cost[v] > cost[u]+w:
-				cost[v] = cost[u]+w
-				parents[v] = u
+			for (v,w) in graph[u]:
+				pq.put( (w,v) )
 
-	return parents				
+
+				if v!=s:
+					if cost[v] == float("inf"):
+						cost[v] = cost[u]+w
+						parents[v] = u
+					else:
+						if cost[v] > cost[u]+w:
+							cost[v] = cost[u]+w
+							parents[v] = u
+
+	return parents, cost
+
+
+g = [[(1,2),(2,4),(4,1)],[(0,2),(2,1)],[(0,4),(1,2),(3,1)],[(2,1),(4,1)],[(0,1),(3,1)]]	
+
+print(dijkstra_shortest_path(g,0))			
 
 
 
