4302135e5f88b61cd25d51794690f7a63436ee8f
Leticia L. Rodriguez <lrodrig@dc.uba.ar>
2022-10-25 15:53:46 -0300

    adding a priority queue for dijkstra algorithm

--- dijkstra_shortest_path.py
+++ dijkstra_shortest_path.py
@@ -1,26 +1,71 @@
-from queue import PriorityQueue
+class PriorityQueue:
+	
+
+	def __init__(self):
+		self.elements = list()
+		
+
+	def size(self):
+		return len(self.elements)
+
+	def push(self, priority, value):
+		if self.size() == 0:
+			self.elements.append( (priority, value) )
+
+		else:
+			p = 0 
+
+			while (p < self.size() and self.elements[p][0] < priority):
+				p+=1
+
+			if p == self.size():
+				self.elements.append( (priority, value))
+			else:
+				self.elements.insert(p, (priority, value))
+
+	def change_priority(self, value, new_priority):
+		p = 0 
+
+		while (p < self.size() and self.elements[p][1] != value):
+			p+=1	
+
+		if p < self.size():
+			del self.elements[p]
+			self.push(new_priority, value)
+
+	def pop(self):
+		v = self.elements[0][1]
+		del self.elements[0]
+		return v
+
+	def empty(self):
+		return len(self.elements) == 0
 
 def dijkstra_shortest_path(graph, s):
 	"""
 	graph is adyacency list where each list has a tuple (v, w) target node and weight
 	"""
 
-	pq = PriorityQueue()
+	n = len(graph)
 	S = set()
+	pq = PriorityQueue()
+	
 
-	pq.put( (0,s) )
+	pq.push(0,s)
+	for x in range(1,n):
+		pq.push(float("inf"),x)
 
 	parents = [None for x in range(len(graph))]
-	cost = [float("inf") for x in range(len(graph))]
+	cost = [ float("inf") for x in range(len(graph))]
 	cost[s] = 0
 	
 	while (not pq.empty()):
-		u = pq.get()[1]
+		u = pq.pop()
 		if u not in S:
 			S.add(u)
 
 			for (v,w) in graph[u]:
-				pq.put( (w,v) )
+				pq.change_priority( v, w )
 
 
 				if v!=s:
